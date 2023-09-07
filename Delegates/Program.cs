1ad62add72c59ab727eb184178dd352471b868fc
ktlhape <ktlhape@iie.ac.za>
2023-09-07 09:10:47 +0200

    delegates

---
+++ Delegates/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        public delegate bool FilterDelegate(int n);
+        static void Main(string[] args)
+        {
+            
+
+            Console.ReadLine();
+        }
+
+        static void DisplayNumbers(string title, int[] nums,FilterDelegate filter)
+        {
+            Console.WriteLine(title);
+            foreach (int num in nums)
+            {
+                if (filter(num)) Console.Write(num + " ");
+            }
+        }
+        static bool IsNegative(int num) => num < 0;
+        static bool IsPositive(int num) => num > 0;
+        static bool IsEven(int num) => num %2 == 0;
+        static bool IsOdd(int num) => num % 2 > 0;
+        
+
+    }
+}
